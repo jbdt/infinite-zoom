43f57fc4f197c415c3c71b41d60f0b1b31a985f9
Javier Blanco <javierblancodetorres@gmail.com>
2024-05-21 19:05:20 +0700

    Small changes

---
+++ script-zoom.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const numImages = 46;
+  const slow = 0.8;
+  const container = document.createElement('div');
+  container.style.display = 'none'; // Ocultamos el contenedor mientras se cargan las imágenes
+  // Agregar las imágenes al contenedor
+  for (let i = 0; i < numImages; i++) {
+    const img = new Image();
+    img.src = `img/forest${i}.jpg`;
+    container.appendChild(img);
+  }
+  // Una vez que todas las imágenes estén cargadas, mostramos el contenedor y comenzamos las animaciones
+  function showContainerAndAnimate() {
+    document.body.appendChild(container); // Agregar el contenedor al DOM
+    for (let i = 0; i < numImages; i++) {
+      const element = document.createElement('div');
+      element.classList.add('zoom');
+      element.style.backgroundImage = `url('img/forest${i}.jpg')`;
+      document.body.appendChild(element); // Agregar el elemento al DOM
+      switch (i) {
+        case 0:
+          addAnimation(element, 0, 0, "aplhaScale");
+          break;
+        case 1:
+          addAnimation(element, 0, 0, "betaScale");
+          break;
+        default:
+          addAnimation(element, slow * 4 * (i - 2), slow * 4 * (i - 2), "finalScale");
+      }
+    }
+  }
+  showContainerAndAnimate();
+
+  function addAnimation(element, delayHidden, delayFinalScale, animationScale) {
+    element.style.animation = `stayHidden ${delayHidden}s, ${animationScale} ${slow * 16}s ${delayFinalScale}s linear `;
+  }
+  setInterval(() => {
+    var zoomDivs = document.querySelectorAll('div.zoom');
+    for (var i = 0; i < zoomDivs.length; i++) {
+      var zoomDiv = zoomDivs[i];
+      zoomDiv.parentNode.removeChild(zoomDiv);
+    }
+    showContainerAndAnimate();
+  }, (numImages * 4 * slow) * 1000);
+});
