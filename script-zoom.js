eb44d3154db4920c8dee99de6fad7cef690833f5
Javier Blanco <javierblancodetorres@gmail.com>
2024-06-01 11:10:17 +0700

    Improve code

--- script-zoom.js
+++ script-zoom.js
@@ -2,14 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
   setTimeout(() => {
     const urlParams = new URLSearchParams(window.location.search);
 
-    const prefix = "frames";
-    const startAt = 0;
+    const prefix = "a";
+    const startAt = 1;
     const extension = "png";
     const images = 4;
     const speed = urlParams.get('speed') || 1;
 
-    const slow = 1 / speed;
-    const numImages = images + 2;
+    const slow = 4 / speed;
     const container = document.createElement('div');
     container.style.display = 'none';
 
@@ -20,31 +19,22 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     for (let i = 0; i < images; i++) {createImages(i);}
-    for (let i = 0; i < 2; i++) {createImages(i);}
 
     function showContainerAndAnimate() {
       document.body.appendChild(container);
-      for (let i = 0; i < numImages; i++) {
+      for (let i = 0; i < images; i++) {
         const element = document.createElement('div');
         element.classList.add('zoom');
-        imgNumber = i + 2 < numImages ? i + startAt : i - numImages + 2;
-        element.style.backgroundImage = `url('img/${prefix}${imgNumber}.${extension}')`;
+        element.style.backgroundImage = `url('img/${prefix}${i + startAt}.${extension}')`;
         document.body.appendChild(element);
-        addAnimation(element, slow * 4 * (i - 2), slow * 4 * (i - 2), "finalScale");
+        addAnimation(element, slow * (i - 4), slow * (i - 4));
       }
     }
     showContainerAndAnimate();
 
-    function addAnimation(element, delayHidden, delayFinalScale, animationScale) {
-      element.style.animation = `stayHidden ${delayHidden}s, ${animationScale} ${slow * 16}s ${delayFinalScale}s cubic-bezier(0.66, 0.5, 0.5, 0.33)`;
+    function addAnimation(element, delayHidden, delayFinalScale) {
+      element.style.animation = `stayHidden ${slow * 4}s ${delayHidden}s infinite,
+        finalScale ${slow * 4}s ${delayFinalScale}s cubic-bezier(0.66, 0.5, 0.5, 0.33) infinite`;
     }
-    setInterval(() => {
-      var zoomDivs = document.querySelectorAll('div.zoom');
-      for (var i = 0; i < zoomDivs.length; i++) {
-        var zoomDiv = zoomDivs[i];
-        zoomDiv.parentNode.removeChild(zoomDiv);
-      }
-      showContainerAndAnimate();
-    }, ((numImages - 2) * 4 * slow) * 1000);
   }, "100");
 });
